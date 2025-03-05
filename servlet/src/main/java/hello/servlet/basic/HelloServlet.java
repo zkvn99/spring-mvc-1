5544ae239ed9c96dd8d843a14f66883e1098dd9a
이민욱 <56884540+zkvn99@users.noreply.github.com>
2025-03-05 19:48:41 +0900

    Hello Servlet

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+
+        String message = req.getParameter("message");
+        System.out.println("message = " + message);
+
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("Hello World" + message);
+    }
+}
